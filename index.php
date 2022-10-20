3ff62e0f04bc31d1bf4b1edf42cbd9bd1fc21fbc
badassboy <emmanuelobeng7788@gmail.com>
2022-10-20 19:34:53 +0100

    changed to lakesidecredit

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
 
 <head>
     <meta charset="utf-8">
-    <title>LemonFirm Bank</title>
+    <title>Lakeside Credit Union</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Financial Service Company" name="keywords">
     <meta content="Best financial service provider for your business" name="description">
