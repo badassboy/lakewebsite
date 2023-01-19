7d510ccb86993d6449e411c7b44d2f2d0b8797e8
badassboy <emmanuelobeng7788@gmail.com>
2023-01-19 13:51:36 +0000

    removing sensitive data

--- config.php
+++ config.php
@@ -2,9 +2,10 @@
 
 // include google client library for PHP autoload file
 require_once 'vendor/autoload.php';
+include("api_keys.php");
+$api_keys = new API();
 
-$client_ID = "214541182768-c4subfnqbimr9rh4f3hopmvrk6gp6lnh.apps.googleusercontent.com";
-$client_secret = "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";
+
 
 // make object of google api client to call Google API
 $google_client = new Google_Client();
@@ -13,9 +14,9 @@ $google_client->setAuthConfig('client_secret.json');
 
 
 // set the OAuth 2.0 client ID
-$google_client->setClientId($client_ID);
+$google_client->setClientId($api_keys->clientID());
 // secret key
-$google_client->setClientSecret($client_secret);
+$google_client->setClientSecret($api_keys->secretkey());
 
 // redurect uri
 $google_client->setRedirectUri('https://lakesidecreditunion.com/user/homepage.php');
